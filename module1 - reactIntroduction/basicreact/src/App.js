f3961ba0a5e545e52303dbf917fd25449ad349bc
Jayasri P <jayaporalla921@gmail.com>
2025-09-22 10:23:04 +0530

    Made changes in module1

--- module1 - reactIntroduction/basicreact/src/App.js
+++ module1 - reactIntroduction/basicreact/src/App.js
@@ -3,18 +3,22 @@ import { useState } from 'react';
 
 function App() {
 
-  const [count, setCount] = useState(0);
-  const [text, setText ] = useState('');
+  const [ count, setCount ] = useState(0);
+  const [ text, setText ] = useState('');
 
   return (
-      <><div>
-      <input type='text' placeholder='Enter text' onChange={(e) => setText(e.target.value)} />
-    </div><div>
-        <p>Hello - {text}</p>
-      </div><div>
+      <>
+      <div>
+        <input type='text' placeholder='Enter your name' onChange={(e) => setText(e.target.value)}/>
+      </div>
+      <div>
+        Hello - {text}
+      </div>
+      <div>
         Count - {count}
-        <button onClick={() => setCount(count + 1)}>Click Me</button>
-      </div></>
+        <button onClick={() => setCount(count + 1)}>Click Me!</button>
+      </div>
+      </>
   );
 }
 
